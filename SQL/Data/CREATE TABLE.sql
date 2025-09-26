db07ec52b74375212de7e5f2483ea7b619be851a
Marlon Claudino <marlon.2204@outlook.com>
2025-09-26 17:08:48 -0300

    Add files via upload

---
+++ SQL/Data/CREATE TABLE.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `seu_projeto.seu_dataset.netflix_titles` (
+    show_id STRING,
+    type STRING,
+    title STRING,
+    director STRING,
+    cast STRING,
+    country STRING,
+    date_added DATE,
+    release_year INT64,
+    rating STRING,
+    duration STRING,
+    listed_in STRING,
+    description STRING
+);
